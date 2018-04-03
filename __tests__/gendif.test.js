052b826926b0146616b0588fccc08474ad32ca04
Andrey Lukin <aimm.legate@gmail.com>
2018-04-03 20:09:55 +0500

    fix test v2

--- __tests__/gendif.test.js
+++ __tests__/gendif.test.js
@@ -4,6 +4,6 @@ import gendif from '../src';
 
 test('plain', () => {
   const expected = '__tests__/__fixtures__/result-plain.txt';
-  const received = gendif('__tests__/__fixtures__/after-plain.json', '__tests__/__fixtures__/before-plain.json');
+  const received = gendif('__tests__/__fixtures__/before-plain.json', '__tests__/__fixtures__/after-plain.json');
   expect(received).toBe(fs.readFileSync(expected, 'utf-8'));
 });
