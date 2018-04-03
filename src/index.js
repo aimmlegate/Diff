e07ac9321376472fc64ed766cf3348b3d4dde7e9
Andrey Lukin <aimm.legate@gmail.com>
2018-04-04 00:30:09 +0500

    fix names, lodash

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 // @flow
 
 import fs from 'fs';
+import _ from 'lodash';
 
-const print = (prefix: string, key: string, val: string) => `${prefix} ${key}: ${val}`;
+const getDiffLine = (prefix: string, key: string, val: string) => `${prefix} ${key}: ${val}`;
 
-const parse = (ast: Object) => {
-  const keys = Object.keys(ast);
+const parse = (jointObj: Object) => {
+  const keys = Object.keys(jointObj);
   const result = keys.reduce((acc, key) => {
-    if (ast[key].oldValue === null) {
-      return `${acc}\n ${print('+', key, ast[key].newValue)}`;
+    if (jointObj[key].oldValue === null) {
+      return `${acc}\n ${getDiffLine('+', key, jointObj[key].newValue)}`;
     }
-    if (ast[key].newValue === null) {
-      return `${acc}\n ${print('-', key, ast[key].oldValue)}`;
+    if (jointObj[key].newValue === null) {
+      return `${acc}\n ${getDiffLine('-', key, jointObj[key].oldValue)}`;
     }
-    if (ast[key].newValue === ast[key].oldValue) {
-      return `${acc}\n ${print(' ', key, ast[key].newValue)}`;
+    if (jointObj[key].newValue === jointObj[key].oldValue) {
+      return `${acc}\n ${getDiffLine(' ', key, jointObj[key].newValue)}`;
     }
-    if (!(ast[key].newValue === ast[key].oldValue)) {
-      const doubleStr = `${print('+', key, ast[key].newValue)}\n ${print('-', key, ast[key].oldValue)}`;
+    if (!(jointObj[key].newValue === jointObj[key].oldValue)) {
+      const doubleStr = `${getDiffLine('+', key, jointObj[key].newValue)}\n ${getDiffLine('-', key, jointObj[key].oldValue)}`;
       return `${acc}\n ${doubleStr}`;
     }
     return '';
@@ -30,11 +31,14 @@ export default (pathToFile1: any, pathToFile2: any) => {
   const before = JSON.parse(fs.readFileSync(pathToFile2).toString());
   const oldKeys = Object.keys(before);
   const newKeys = Object.keys(after);
-  const metaKeys = new Set([...oldKeys, ...newKeys]);
-  const ast = [...metaKeys].reduce((acc, key) => {
-    const inner = { oldValue: (after[key]) || null, newValue: (before[key] || null) };
+  const uniqKeys = new Set([...oldKeys, ...newKeys]);
+  const jointObj = [...uniqKeys].reduce((acc, key) => {
+    const inner = {
+      oldValue: (_.has(after, key)) ? after[key] : null,
+      newValue: (_.has(before, key)) ? before[key] : null,
+    };
     return { ...acc, [key]: inner };
   }, {});
-  return parse(ast);
+  return parse(jointObj);
 };
 
