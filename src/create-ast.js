eb6e8a517a2aaff21769a1446ae3033f1354d4ad
Andrey Lukin <aimm.legate@gmail.com>
2018-04-06 02:15:16 +0500

    revorked ast

--- src/create-ast.js
+++ src/create-ast.js
@@ -6,47 +6,44 @@ import readFile from './read-file';
 
 
 const buildAst = (before, after) => {
-  const build = (bef, aft) => {
-    const keys = _.union(_.keys(bef), _.keys(aft));
-    return keys.reduce((acc, key) => {
-      if (!(_.isObject(bef[key]) && _.isObject(aft[key]))) {
-        const node = {
-          key,
-          value: (_.has(aft, key)) ? aft[key] : null,
-          oldValue: (_.has(bef, key)) ? bef[key] : null,
-          children: [],
-        };
-        return [...acc, node];
-      }
-      const node = {
-        key,
-        value: null,
-        oldValue: null,
-        children: build(bef[key], aft[key]),
-      };
-      return [...acc, node];
-    }, []);
-  };
-  return build(before, after);
+  const keys = _.union(_.keys(before), _.keys(after));
+  return keys.map((key) => {
+    if (_.isObject(before[key]) && _.isObject(after[key])) {
+      return { key, type: 'nested', children: buildAst(before[key], after[key]) };
+    }
+    if (!_.has(before, key)) {
+      return { key, type: 'added', value: after[key] };
+    }
+    if (!_.has(after, key)) {
+      return { key, type: 'deleted', value: before[key] };
+    }
+    if (before[key] === after[key]) {
+      return { key, type: 'unchanged', value: before[key] };
+    }
+    return { key, type: 'updated', value: after[key], oldValue: before[key] };
+  });
 };
 
+const stringify = val => JSON.stringify(val);
+
+
 const parseAst = (ast) => {
-  const parseNode = (node, str = '') => {
+  const parseNode = (node, deep = ' ') => {
     if (node.children.length === 0) {
       if (node.value === node.oldValue) {
-        return `${str}\n${node.key}:${node.value}`;
+        return `${deep}   ${node.key}: ${stringify(node.value)}\n`;
       }
       if (node.value === null) {
-        return `${str}\n - ${node.key}:${node.oldValue}`;
+        return `${deep} - ${node.key}: ${stringify(node.oldValue)}\n`;
       }
       if (node.oldValue === null) {
-        return `${str}\n + ${node.key}:${node.value}`;
+        return `${deep} + ${node.key}: ${stringify(node.value)}\n`;
       }
-      return `${str}\n + ${node.key}:${node.value}\n - ${node.key}:${node.oldValue}`;
+      return `${deep} + ${node.key}: ${stringify(node.value)}\n${deep} - ${node.key}: ${stringify(node.oldValue)}\n`;
     }
-    return node.children.reduce((acc, el) => (`${acc} ${parseNode(el)}`), str);
+    return node.children.reduce((acc, el) => (`${acc}\n ${parseNode(el, deep.repeat(2))}`), '');
   };
-  return ast.reduce((acc, el) => (`${acc} ${parseNode(el, acc)}`), '');
+  return ast.reduce((acc, el) => (`${el.key}: ${parseNode(el, acc)}`), '');
 };
 
 export default (path1: string, path2: string) => {
@@ -54,6 +51,7 @@ export default (path1: string, path2: string) => {
   const confAfterFile = readFile(path2);
   const before = getParse(confBeforeFile.ext)(confBeforeFile.data);
   const after = getParse(confAfterFile.ext)(confAfterFile.data);
+  console.log(stringify(buildAst(before, after)));
   return parseAst(buildAst(before, after));
 };
 
