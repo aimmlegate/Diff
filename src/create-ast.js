192e54b8795a2a8e3104feb5b4d0e27b6ad87fa2
Andrey Lukin <aimm.legate@gmail.com>
2018-04-06 16:57:14 +0500

    not working

--- src/create-ast.js
+++ src/create-ast.js
@@ -20,30 +20,40 @@ const buildAst = (before, after) => {
     if (before[key] === after[key]) {
       return { key, type: 'unchanged', value: before[key] };
     }
-    return { key, type: 'updated', value: after[key], oldValue: before[key] };
+    return {
+      key,
+      type: 'updated',
+      value: after[key],
+      oldValue: before[key],
+    };
   });
 };
 
-const stringify = val => JSON.stringify(val);
+const infoChar = { add: '+', remov: '-', none: ' ' };
 
+const createLine = (val, deep = ' ') => {
+  if (!_.isObject(val)) return val;
+  const keys = _.keys(val);
+  const values = keys.map(key =>
+    `${deep.repeat(2)}${infoChar.none}${key}: ${val[key]}`).join('\n');
+  return `{\n${values}\n}`;
+};
 
-const parseAst = (ast) => {
-  const parseNode = (node, deep = ' ') => {
-    if (node.children.length === 0) {
-      if (node.value === node.oldValue) {
-        return `${deep}   ${node.key}: ${stringify(node.value)}\n`;
-      }
-      if (node.value === null) {
-        return `${deep} - ${node.key}: ${stringify(node.oldValue)}\n`;
-      }
-      if (node.oldValue === null) {
-        return `${deep} + ${node.key}: ${stringify(node.value)}\n`;
-      }
-      return `${deep} + ${node.key}: ${stringify(node.value)}\n${deep} - ${node.key}: ${stringify(node.oldValue)}\n`;
-    }
-    return node.children.reduce((acc, el) => (`${acc}\n ${parseNode(el, deep.repeat(2))}`), '');
+const renderAst = (ast, deps = ' ') => {
+  const selectLine = {
+    added: node => [`${deps}${infoChar.add} ${node.key}: ${createLine(node.value, deps)}`],
+    deleted: node => [`${deps}${infoChar.remov} ${node.key}: ${createLine(node.value, deps)}`],
+    update: node => [
+      `${deps}${infoChar.add} ${node.key}: ${createLine(node.value, deps)}`,
+      `${deps}${infoChar.remov} ${node.key}: ${createLine(node.oldValue, deps)}`,
+    ],
+    unchanged: node => [`${deps}${infoChar.none} ${node.key}: ${createLine(node.children, deps)}`],
+    nested: node => [`${deps}${infoChar.none} ${node.key}: ${renderAst(node.children, deps.repeat(2))}`],
   };
-  return ast.reduce((acc, el) => (`${el.key}: ${parseNode(el, acc)}`), '');
+  const result = ast.reduce((acc, node) => {
+    return [...acc, selectLine[node.type](node)];
+  }, []);
+  return `{\n${_.flatten(result).join('\n')}\n}`;
 };
 
 export default (path1: string, path2: string) => {
@@ -51,7 +61,7 @@ export default (path1: string, path2: string) => {
   const confAfterFile = readFile(path2);
   const before = getParse(confBeforeFile.ext)(confBeforeFile.data);
   const after = getParse(confAfterFile.ext)(confAfterFile.data);
-  console.log(stringify(buildAst(before, after)));
-  return parseAst(buildAst(before, after));
+  //console.log(JSON.stringify(buildAst(before, after)));
+  console.log(renderAst(buildAst(before, after)));  
 };
 
